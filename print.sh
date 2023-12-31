66cda36fc24237d8f1ddb71beaeb4e14012b98db
Raoul C <raoulconstant@gmail.com>
2023-12-30 23:37:26 -0700

    created shell script for back end

--- print.sh
+++ print.sh
@@ -1,1 +1,23 @@
 echo Hello World
+
+## color printing, we have 6 colors
+# Red        -31
+# Green      -32
+# Yellow     -33
+# Blue       -34
+# Magenta    -35
+# Cyan       -36
+
+
+
+# echo -e "\e[COLCODEmMessage\e[0m"
+
+echo -e "\e[31mHello World\e[0m"
+echo -e "\e[32mHello World\e[0m"
+echo -e "\e[33mHello World\e[0m"
+echo -e "\e[34mHello World\e[0m"
+echo -e "\e[35mHello World\e[0m"
+echo -e "\e[36mHello World\e[0m"
+
+
+
