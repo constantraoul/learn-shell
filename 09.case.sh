690f235ae3047ba54bd1117c1c4b6bee23dd67e7
Raoul C <raoulconstant@gmail.com>
2024-01-14 04:24:36 -0700

    Shell Code

---
+++ 09.case.sh
@@ -0,0 +1,12 @@
+# If you want to compare strings, then case comes handy.
+
+course=$1
+
+case $course in
+AWS)
+  echo Welcome to AWS Training
+  ;;
+DevOps)
+  echo Welcome to DevOps Training
+  ;;
+esac
